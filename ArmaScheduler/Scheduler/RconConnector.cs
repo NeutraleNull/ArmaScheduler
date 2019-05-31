c6b96f3ad0f4dafc4c43322fd37fe01c813a10ba
Oliver Krüppel <oliverkrue@gmail.com>
2019-06-01 00:01:46 +0200

    reworked RCON connector to avoid deadlock and programm crash

--- ArmaScheduler/Scheduler/RconConnector.cs
+++ ArmaScheduler/Scheduler/RconConnector.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Threading.Tasks;
 using ArmaSheduler.BattleNET;
 using ArmaScheduler.Models;
+using System.Collections.Generic;
 
 namespace ArmaScheduler.Scheduler
 {
@@ -11,7 +12,11 @@ namespace ArmaScheduler.Scheduler
         private static RconConnector _connector;
         private Settings _settings;
         private BattlEyeClient _client;
+        private Queue<string> queue = new Queue<string>();
         private readonly object _lock = new object();
+        private readonly object _lockQueue = new object();
+        private bool _queueWorker = false;
+        private bool _busy = false;
 
         private RconConnector()
         {
@@ -31,6 +36,7 @@ namespace ArmaScheduler.Scheduler
         {
             Task.Run(() =>
             {
+                _busy = true;
                 Console.WriteLine("Connection to the server has been lost...\n Will try to reconnect now!");
                 for (int tries = 0; tries < _settings.repeat; tries++)
                 {
@@ -39,6 +45,7 @@ namespace ArmaScheduler.Scheduler
                     if (result == 0)
                     {
                         Console.WriteLine("Connection reestablished...");
+                        _busy = false;
                         return;
                     }
                     Console.WriteLine("Connection could not be reestablished...");
@@ -73,50 +80,105 @@ namespace ArmaScheduler.Scheduler
 
         public int OpenConnection()
         {
+            _busy = true;
             lock (_lock)
             {
                 var result = _client.Connect();
                 if (result != 0)
                 {
-                    Console.WriteLine("Connection to server failed");
-                }
-
-                Task.Run(() =>
-                {
-                    Console.WriteLine("Connection to the server has been lost...\n Will try to reconnect now!");
-                    for (int tries = 0; tries < _settings.repeat; tries++)
+                    Task.Run(() =>
                     {
-                        Console.WriteLine($"Try {tries}/{_settings.repeat}");
-                        result = _client.Connect();
-                        if (result == 0)
+                        _busy = true;
+                        Console.WriteLine("Connection to the server could not be established...\n Will try to reconnect now!");
+                        for (int tries = 0; tries < _settings.repeat; tries++)
                         {
-                            Console.WriteLine("Connection established...");
-                            return;
+                            Console.WriteLine($"Try {tries}/{_settings.repeat}");
+                            result = _client.Connect();
+                            if (result == 0)
+                            {
+                                Console.WriteLine("Connection established...");
+                                _busy = false;
+                                return;
+                            }
+                            Console.WriteLine("Connection could not be established...");
+                            Task.Delay(5 * 1000).Wait();
                         }
-                        Console.WriteLine("Connection could not be established...");
-                        Task.Delay(5 *1000).Wait();
-                    }
-                    Environment.Exit(1);
-                });
+                        Environment.Exit(1);
+                    });
+                }
+                _busy = false;
                 return (int)result;
             }
         }
 
-        public void SendCommand(string command)
+        private void SendCommand(string command)
         {
-            lock(_lock)
+            if(_client.Connected)
             {
-                if (_client.Connected)
-                    _client.SendCommand(command, true);
-                else
+                lock(_lock)
                 {
-                    OpenConnection();
-                    if (_client.Connected)
-                        _client.SendCommand(command, true);
-                    else
-                        Console.WriteLine("Cannot send command... Not connected to server");
+                    try
+                    {
+                        _client.SendCommand(command);
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine($"Something went wrong: {ex}");
+                    }
                 }
             }
+            else
+            {
+                OpenConnection();
+                SendCommand(command);
+            }
+        }
+
+        public void AddCommandToQueue(string command)
+        {
+            if (string.IsNullOrEmpty(command))
+            {
+                throw new ArgumentException("message", nameof(command));
+            }
+            lock (_lockQueue)
+            {
+                queue.Enqueue(command);
+            }
+        }
+
+        public void StartQueueWorker()
+        {
+            if (_queueWorker) return;
+            _queueWorker = true;
+            Task.Run(() =>
+            {
+                while(true)
+                {
+                    if(queue.Count > 0)
+                    {
+                        string command = "";
+                        lock(_lockQueue)
+                        {
+                            command = queue.Dequeue();
+                        }
+                        if(!_busy)
+                        {
+                            Console.WriteLine("Sending command: "+command);
+                            _client.SendCommand(command);
+                        }
+                        else
+                        {
+                            Console.WriteLine("RCON currently busy... delaying command");
+                            lock(_lockQueue)
+                            {
+                                queue.Enqueue(command);
+                            }
+                        }
+                    }
+                    Task.Delay(2000).Wait();
+                }
+            });
+
         }
 
         public void CloseConnection()
