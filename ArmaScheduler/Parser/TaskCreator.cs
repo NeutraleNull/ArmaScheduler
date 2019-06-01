4186adb379fb5439df16138355f2ce72034157ba
Oliver Krüppel <oliverkrue@gmail.com>
2019-06-01 10:28:11 +0200

    fixed background task would overwrite themself due to same taskid

--- ArmaScheduler/Parser/TaskCreator.cs
+++ ArmaScheduler/Parser/TaskCreator.cs
@@ -14,17 +14,18 @@ namespace ArmaScheduler.Parser
         {
             //var server = SchedulerServer.GetInstance();
             _ = BackgroundJob.Enqueue(() => Console.WriteLine("Starting scheduler..."));
-
+            int index = 0;
             foreach (var item in model.scheduledTasks)
             {
                 if (item.rconCommand != null)
                 {
-                    RecurringJob.AddOrUpdate<TaskService>(x => x.ExecuteRcon(item.rconCommand), string.Format("{0} {1} * * *", item.time.Minutes, item.time.Hours), TimeZoneInfo.Local);
+                    RecurringJob.AddOrUpdate<TaskService>(string.Format("{0}rconCommand", index), x => x.ExecuteRcon(item.rconCommand), string.Format("{0} {1} * * *", item.time.Minutes, item.time.Hours), TimeZoneInfo.Local);
                 }
                 if (item.executeTask != ExecutionTasks.none)
                 {
-                    RecurringJob.AddOrUpdate<TaskService>(x => x.RestartServer(item.executeTask), string.Format("{0} {1} * * *", item.time.Minutes, item.time.Hours), TimeZoneInfo.Local);
+                    RecurringJob.AddOrUpdate<TaskService>(string.Format("{0}executeTask", index), x => x.RestartServer(item.executeTask), string.Format("{0} {1} * * *", item.time.Minutes, item.time.Hours), TimeZoneInfo.Local);
                 }
+                index++;
             }
      
             foreach (var item in model.repeatingServices)
